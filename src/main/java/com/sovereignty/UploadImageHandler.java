2a63114f40c6d33760f7b457da3e316c4f44515e
curiouslemur <>
2019-11-24 18:42:35 -0500

    class not found again

--- src/main/java/com/sovereignty/UploadImageHandler.java
+++ src/main/java/com/sovereignty/UploadImageHandler.java
@@ -19,6 +19,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.sovereignty.db.VisualElementDAO;
 import com.sovereignty.http.UploadImageRequest;
 import com.sovereignty.http.UploadImageResponse;
 
@@ -32,22 +33,21 @@ public class UploadImageHandler implements RequestHandler<UploadImageRequest, Up
     private final String PNG_MIME = (String) "image/png";
     
     private AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
+    VisualElementDAO veDAO = new VisualElementDAO();
     
     public UploadImageHandler() {}
 
-    UploadImageHandler(AmazonS3 s3) {
-        this.s3 = s3;
-    }
+    UploadImageHandler(AmazonS3 s3) { this.s3 = s3; }
 
     @Override
     public UploadImageResponse handleRequest(UploadImageRequest event, Context context) {
-        context.getLogger().log("Received event: " + event);
-
+        context.getLogger().log("Received image to be uploaded: " + event.getImage64());
+        
 // Get the object from the event and show its content type
 //        String bucket = event.getRecords().get(0).getS3().getBucket().getName();
 //        String key = event.getRecords().get(0).getS3().getObject().getKey();
        
-        String bucket = "Sovereignty-cms/images";
+        String bucket = "sovereignty-cms/images";
         String key = event.getImageName();
         
      // Infer the image type.
@@ -79,15 +79,17 @@ public class UploadImageHandler implements RequestHandler<UploadImageRequest, Up
             
         	ImageIO.write(srcImage, imageType, os);
         	
-        	// write the file to s3
+        	
         	try {
+        		// write the file to s3        		
                 s3.putObject(bucket, key, is, meta);
+                // TODO: create a visualElement to store? 
                 return new UploadImageResponse(200, "image uploaded");
             }
             catch(AmazonServiceException e) {
                 System.err.println(e.getErrorMessage());
                 System.exit(1);
-    			return new UploadImageResponse(500, "Can't upload image. AmazonServiceException Error: "+ e.getMessage());
+    			return new UploadImageResponse(500, "Can't upload image to s3. AmazonServiceException Error: "+ e.getMessage());
             }
 		} catch (IOException e1) {
 			e1.printStackTrace();
