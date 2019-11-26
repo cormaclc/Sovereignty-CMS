b888dbd8f0ffd415b7e82d1a47c29421d0a02537
Cormac Lynch-Collier <cormac@autoreg-231161.dyn.wpi.edu>
2019-11-26 17:38:07 -0500

    remove orientation from duplicate

--- front-end/src/Add/DuplicateCard.js
+++ front-end/src/Add/DuplicateCard.js
@@ -10,12 +10,10 @@ function DuplicateCard(props) {
     const [show, setShow] = React.useState(false)
     const [recipient, setRecipient] = React.useState('')
     const [eventType, setEventType] = React.useState('Anniversary')
-    const [orientation, setOrientation] = React.useState('Landscape')    
 
     React.useEffect(() => {
         setRecipient(props.card.recipient)
         setEventType(props.card.eventType);
-        setOrientation(props.card.orientation);
         setShow(props.show);
     }, [props.card, props.show])
 
@@ -23,13 +21,11 @@ function DuplicateCard(props) {
 
     const handleRecipientChange = (e) => { setRecipient(e.target.value) }
     const handleEventChange = (e) => { setEventType(e.target.value) } 
-    const handleOrientationChange = (e) => { setOrientation(e.target.value) }
 
     const handleDuplicateCard = () => {
         let c = props.card;
         c.recipient = recipient
         c.eventType = eventType
-        c.orientation = orientation
     
         props.duplicateCard(c);
 
@@ -60,14 +56,7 @@ function DuplicateCard(props) {
                                 })}
                             </Form.Control>
                         </Form.Group>
-
-                        <Form.Group controlId="form-orientation">
-                            <Form.Label>Card Orientation</Form.Label>
-                            <Form.Control required as="select" onChange={handleOrientationChange}>
-                                <option defaultValue={'Landscape' === orientation}>Landscape</option>
-                                <option defaultValue={'Portrait' === orientation}>Portrait</option>
-                            </Form.Control>
-                        </Form.Group>
+                        
                     </Form>
                 </Modal.Body>
                 <Modal.Footer>
