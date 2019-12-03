2c39b78f027d1721a0b55ce6074ac2e6eb02db4a
Noëlle R <noelle.tiana@gmail.com>
2019-12-03 12:30:04 -0500

    fixed upload card return

--- src/main/java/com/sovereignty/UploadImageHandler.java
+++ src/main/java/com/sovereignty/UploadImageHandler.java
@@ -1,7 +1,14 @@
 package com.sovereignty;
 
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.InputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.imageio.ImageIO;
 
 import org.apache.commons.codec.binary.Base64;
 
@@ -18,28 +25,26 @@ import com.sovereignty.http.UploadImageRequest;
 import com.sovereignty.http.UploadImageResponse;
 import com.sovereignty.model.Image;
 
-//public class UploadImageHandler implements RequestHandler<S3Event, String> {
 public class UploadImageHandler implements RequestHandler<UploadImageRequest, UploadImageResponse> {
-    private final String JPG_TYPE = (String) "jpg";
-    private final String JPG_MIME = (String) "image/jpeg";
-    private final String PNG_TYPE = (String) "png";
-    private final String PNG_MIME = (String) "image/png";
-    
-    ImageDAO dao = new ImageDAO();
-    
-    public UploadImageHandler() {}
+	private final String JPG_TYPE = (String) "jpg";
+	private final String JPG_MIME = (String) "image/jpeg";
+	private final String PNG_TYPE = (String) "png";
+	private final String PNG_MIME = (String) "image/png";
 
-//    UploadImageHandler(AmazonS3 s3) { this.s3 = s3; }
+	ImageDAO dao = new ImageDAO();
 
-    public UploadImageResponse handleRequest(UploadImageRequest input, Context context) {
-        context.getLogger().log("Received image to be uploaded: " + input.getImageName());
-              
-        String bucketName = "sovereignty-images";
-        String bucketURL = "https://sovereignty-images.s3.amazonaws.com/";
-        
-        String key = input.getImageName()+ "-" + input.getImageID();
-	    String imageURL = bucketURL + key ;
-        
+	public UploadImageHandler() {
+	}
+
+	public UploadImageResponse handleRequest(UploadImageRequest input, Context context) {
+		context.getLogger().log("Received image to be uploaded: " + input.getImageName());
+
+		String bucketName = "sovereignty-images";
+		String bucketURL = "https://sovereignty-images.s3.amazonaws.com/";
+
+		String key = input.getImageName() + "-" + input.getImageID();
+		String imageURL = bucketURL + key;
+
 // Infer the image type.
 		/*
 		 * Matcher matcher =
@@ -49,49 +54,43 @@ public class UploadImageHandler implements RequestHandler<UploadImageRequest, Up
 		 * imageType = matcher.group(1); if (!(JPG_TYPE.equals(imageType)) &&
 		 * !(PNG_TYPE.equals(imageType))) { System.out.println("Skipping non-image " +
 		 * key); }
-		 */        
+		 */
+		 
 // base64 to imageFile (imageByteArray)
-        byte[] imageByteArray = Base64.decodeBase64(input.getImage64());
-        ObjectMetadata meta = new ObjectMetadata();
-        
+		byte[] imageByteArray = Base64.decodeBase64(input.getImage64());
+		ObjectMetadata meta = new ObjectMetadata();
+
 // Set Content-Length and Content-Type
 //      meta.setContentLength(os.size());
-		//meta.setContentLength(input.getImage64().length());
+		// meta.setContentLength(input.getImage64().length());
 		/*
 		 * if (JPG_TYPE.equals(imageType)) { meta.setContentType(JPG_MIME); } if
 		 * (PNG_TYPE.equals(imageType)) { meta.setContentType(PNG_MIME); }
 		 */
 		InputStream inputStream = new ByteArrayInputStream(imageByteArray);
-		
-//      BufferedImage srcImage = ImageIO.read(new ByteArrayInputStream(imageByteArray));
-//      
-//      ByteArrayOutputStream os = new ByteArrayOutputStream();
-//      ImageIO.write(srcImage, imageType, os);
-//      InputStream is = new ByteArrayInputStream(os.toByteArray());
-//      ObjectMetadata meta = new ObjectMetadata();
-		
-		try {
-		    AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
-		    PutObjectRequest request = new PutObjectRequest(bucketName, key, inputStream, meta);
-		    s3.putObject(request.withCannedAcl(CannedAccessControlList.PublicRead));
-		    
-		    //s3.putObject(bucketName, key, input.getImage64());
-			s3.putObject(bucketName, key, inputStream, meta);
 
+		try {
+					
+			AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
+			PutObjectRequest request = new PutObjectRequest(bucketName, key, inputStream, meta);
+			s3.putObject(request.withCannedAcl(CannedAccessControlList.PublicRead));
+			s3.putObject(bucketName, key, inputStream, meta);
 			Image img = new Image(input.getImageID(), key, imageURL);
-		    try {
+
+			try {
 				dao.addImage(img);
+				UploadImageResponse res = new UploadImageResponse(200, "image uploaded", imageURL);
+				return res;
 			} catch (Exception e) {
 				System.out.println("---------------");
 				e.printStackTrace();
-				return new UploadImageResponse(500, "Can't add image to database, Error: "+e.getMessage());
-			}		    
-		    
-		    return new UploadImageResponse(200, "image uploaded");
-		    
-		} catch(Exception e) {
-		    System.err.println(e.getMessage());
-			return new UploadImageResponse(500, "Can't upload image to s3. AmazonServiceException Error: "+ e.getMessage());
+				UploadImageResponse res = new UploadImageResponse(500, "Can't add image to database, Error: " + e.getMessage());
+				return res;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new UploadImageResponse(500, "Can't upload image to s3. AmazonServiceException Error: " + e.getMessage());
 		}
-    }
+	}
 }
--- src/main/java/com/sovereignty/http/UploadImageResponse.java
+++ src/main/java/com/sovereignty/http/UploadImageResponse.java
@@ -2,10 +2,42 @@ package com.sovereignty.http;
 
 public class UploadImageResponse {
 	int code;
-	String errorMessage;
+	String error;
+	String imageURL;
 	
 	public UploadImageResponse(int code, String errorMessage) {
 		this.code = code;
-		this.errorMessage = errorMessage;
+		this.error = errorMessage;
 	}
+	
+	public UploadImageResponse(int code, String errorMessage, String url) {
+		this.code = code;
+		this.error = errorMessage;
+		this.imageURL = url;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getErrorMessage() {
+		return error;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.error = errorMessage;
+	}
+
+	public String getImageURL() {
+		return imageURL;
+	}
+
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+	
 }
